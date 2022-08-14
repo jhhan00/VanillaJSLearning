2fb5ae013ac46327121de9e171762123b3d5bbe9
jhhan00 <jhhan009@gmail.com>
2022-08-15 00:04:25 +0900

    quotes.js 추가 - 랜덤한 명언을 가져올 수 있도록 설정하기

---
+++ js/quotes.js
@@ -0,0 +1,61 @@
+const quotes = [
+    {
+        quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+        author: "Nelson Mandela"
+    },
+    {
+        quote: "In the end, it's not the years in your life that count. It's the life in your years.",
+        author: "Abraham Lincoln"
+    },
+    {
+        quote: "Life is either a daring adventure or nothing at all.",
+        author: "Helen Keller"
+    },
+    {
+        quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.",
+        author: "Thomas A. Edison"
+    },
+    {
+        quote: "All you need in this life is ignorance and confidence; then success is sure.",
+        author: "Mark Twain"
+    },
+    {
+        quote: "I don't go by the rule book…I lead from the heart, not the head.",
+        author: "Princess Diana"
+    },
+    {
+        quote: "If you spend too much time thinking about a thing, you’ll never get it done.",
+        author: "Bruce Lee"
+    },
+    {
+        quote: "Keep your eyes on the stars and your feet on the ground.",
+        author: "Theodore Roosevelt"
+    },
+    {
+        quote: "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+        author: "Winston S. Churchill"
+    },
+    {
+        quote: "The way to get started is to quit talking and begin doing.",
+        author: "Walt Disney"
+    },
+    {
+        quote: "If you really look closely, most overnight successes took a long time.",
+        author: "Steve Jobs"
+    },
+    {
+        quote: "Life is what happens when you're busy making other plans",
+        author: "John Lennon"
+    },
+    {
+        quote: "a",
+        author: "b"
+    },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+const todayQuuote = quotes[Math.floor(Math.random() * quotes.length)];
+quote.innerText = todayQuuote.quote;
+author.innerText = todayQuuote.author;
