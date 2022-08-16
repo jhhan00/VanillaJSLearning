1dc4f11ca17e5dc4e5470098e3591482848b1b86
jhhan00 <jhhan009@gmail.com>
2022-08-16 22:19:48 +0900

    로그인 전후로 보이는 내용 바뀌도록 더 수정

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const todo_form = document.getElementById("todo-form");
+const todo_list = document.getElementById("todo-list");
+const quutoe = document.getElementById("quote");
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -20,14 +23,17 @@ function paintGreetings(username) {
     // greeting.innerText = "Hello " + username;
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    todo_form.classList.remove(HIDDEN_CLASSNAME);
+    todo_list.classList.remove(HIDDEN_CLASSNAME);
+    quutoe.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 if(savedUsername === null) {
-    // showing the form
+    // show login-form
     loginForm.classList.remove(HIDDEN_CLASSNAME);
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
-    // shwowing the h1
+    // hide login-form
     paintGreetings(savedUsername);
 }
